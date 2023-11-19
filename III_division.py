5ed0420274ffec3c941438cce97d64a5aca5e1c7
JoseAngHdz <2010639@utmatamoros.edu.mx>
2023-11-19 16:36:29 -0600

    Se agrego el algoritmo de devision de 2 numeros y la obtencion del cuadrado y cubo de un numero

---
+++ III_division.py
@@ -0,0 +1,8 @@
+def division_dos_numeros():
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    if num2 != 0:
+        resultado = num1 / num2
+        print(f"La división de {num1} entre {num2} es: {resultado}")
+    else:
+        print("No se puede dividir entre cero.")
---
+++ VI_cubo_y_cuadrado.py
@@ -0,0 +1,6 @@
+def calcular_cuadrado_y_cubo():
+    num = float(input("Ingrese un número para calcular su cuadrado y cubo: "))
+    cuadrado = num ** 2
+    cubo = num ** 3
+    print(f"El cuadrado de {num} es: {cuadrado}")
+    print(f"El cubo de {num} es: {cubo}")
