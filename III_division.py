49f4ea7e3bad9c4601a74a49e6fa9b21d5c97640
JoseAngHdz <2010639@utmatamoros.edu.mx>
2023-11-19 17:34:56 -0600

    se añadio un mecanismo al momento de introducir un 0 como dividendo asi como de un ciclo while que pide otro numero para el dividendo

--- III_division.py
+++ III_division.py
@@ -1,8 +1,10 @@
 def division_dos_numeros():
     num1 = float(input("Ingrese el primer número: "))
     num2 = float(input("Ingrese el segundo número: "))
-    if num2 != 0:
-        resultado = num1 / num2
-        print(f"La división de {num1} entre {num2} es: {resultado}")
-    else:
-        print("No se puede dividir entre cero.")
+    if num2 == 0:
+        while num2 == 0:
+            print("No se puede dividir entre cero.")
+            num2 = float(input("Ingrese el segundo número: "))
+            
+    resultado = num1 / num2
+    print(f"La división de {num1} entre {num2} es: {resultado}")
