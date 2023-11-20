f2b4366238d9beac01ed77063aa4a00fcec376ce
DeylaHdz <deylahdzm11@gmail.com>
2023-11-20 17:12:13 -0600

    Correcion de nombre de la funcion

--- II_Producto.py
+++ II_Producto.py
@@ -1,11 +1,11 @@
 
-def multiplicar():
+def producto():
     try:
         n = int(input("Ingresa la cantidad de números a multiplicar: "))
         producto= 0
 
         for i in range(n):
-            numero = float(input(f"Ingrese el numero {i+1}: "))
+            numero = float(input(f"Ingrese el número {i+1}: "))
             producto = producto + numero
 
         print(f"El producto de los {n} números ingresados es: {producto} ")
