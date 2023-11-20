1e41c2c424363f2b27868212221d06136e756da7
DeylaHdz <deylahdzm11@gmail.com>
2023-11-20 16:55:02 -0600

    Codigo 2 obtencion de producto al multiplicar n numeros

---
+++ II_Producto.py
@@ -0,0 +1,14 @@
+
+def multiplicar():
+    try:
+        n = int(input("Ingresa la cantidad de números a multiplicar: "))
+        producto= 0
+
+        for i in range(n):
+            numero = float(input(f"Ingrese el numero {i+1}: "))
+            producto = producto + numero
+
+        print(f"El producto de los {n} números ingresados es: {producto} ")
+
+    except ValueError:
+        print("Por favor, ingrese un número válido.")
