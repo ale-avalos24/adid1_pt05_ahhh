46669012abd4292820267cced7edf222b6dff258
ale-avalos24 <alejandroavalos2401@gmail.com>
2023-11-20 19:19:34 -0600

    Se crean las opciones 4 y 8 del programa

--- iv_factorial.py
+++ iv_factorial.py
@@ -1,2 +1,6 @@
-def factorial():
-    print("Hola mundo")
+from math import factorial
+    
+def obt_factorial():
+    num = int(input("Ingrese el numero a sacar factorial: "))
+    res = factorial(num)
+    print("\nEl factorial de {} es igual a {}".format(num, res))
